d813b8a815dd4a2b53053e71b69da8bfb599901e
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-05-17 00:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+cosnt target = document.getElementById('level')
+
+let level = 0
+let target = level
+
+while(level){
+	level++
+	current = current.parentElement
+}
+
+alert("The level of the DOM: " + level)
+
